6adee6a3ad1921508b37db476e258f106dca7dc3
sebastian <sebastian.hartwig@uni-ulm.de>
2022-09-22 10:48:22 +0200

    parse checkpoint update

--- pytorch_utils/__init__.py
+++ pytorch_utils/__init__.py
@@ -3,7 +3,7 @@ from pathlib import Path
 def parse_ckpt(path, return_first=True):
     ckpts = [p.as_posix() for p in Path(path).glob("**/*") if p.suffix == ".ckpt"]
     if return_first:
-        ckpt = ckpts[0].as_posix()
+        ckpt = ckpts[0]
         print("Loading checkpoint: ", ckpt)
         return ckpt
     else:
