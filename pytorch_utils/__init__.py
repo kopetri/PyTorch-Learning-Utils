e360e151b3f6066f243e0ef9059da2095f092764
sebastian <sebastian.hartwig@uni-ulm.de>
2023-04-05 14:55:33 +0200

    add generate_splits

--- pytorch_utils/__init__.py
+++ pytorch_utils/__init__.py
@@ -1,5 +1,43 @@
 from pathlib import Path
 import torch
+import numpy as np
+
+def generate_splits(directory, suffix, train=0.8, valid=0.05, test=0.15, shuffle=True):
+    if isinstance(suffix, str):
+        suffix = [suffix]
+    assert train + valid + test == 1.0, train + valid + test
+    directory = Path(directory)
+    print("Generating splits...")
+    def __write_files__(files, out):
+        if isinstance(out, Path):
+            out = out.as_posix()
+        with open(out, "w") as txtfile:
+            for f in sorted(files):
+                txtfile.write("{}\n".format(f.relative_to(directory).as_posix()))
+
+    files = [f for f in directory.rglob("*") if f.suffix in suffix]
+
+    N = len(files)
+    n_train = int(N * train)
+    n_valid = int(N * valid)
+    n_test  = int(N * test)
+
+    rest = N - (n_train + n_valid + n_test)
+    n_test += rest
+
+    assert n_train + n_valid + n_test == N, "{} !+ {}".format(n_train + n_valid + n_test, N)
+
+    if shuffle:
+        np.random.shuffle(files)
+
+    train_split = files[0:n_train]
+    valid_split = files[n_train:n_train+n_valid]
+    test_split  = files[n_train+n_valid:n_train+n_valid+n_test]
+
+    __write_files__(train_split, Path(directory, "train.txt"))
+    __write_files__(valid_split, Path(directory, "valid.txt"))
+    __write_files__(test_split,  Path(directory,  "test.txt"))
+
 
 def parse_ckpt(path, return_first=True):
     if Path(path).is_file():
