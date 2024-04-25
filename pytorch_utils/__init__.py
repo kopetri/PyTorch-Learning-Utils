525038a1521feec0ea20804f546e7e90234ff5f0
Sebastian <sebastian.hartwig@uni-ulm.de>
2024-04-25 08:34:34 +0200

    Update __init__.py

--- pytorch_utils/__init__.py
+++ pytorch_utils/__init__.py
@@ -48,7 +48,7 @@ def parse_ckpt(path, return_first=True, pattern=None):
             return [path]
     ckpts = [p.as_posix() for p in Path(path).glob("**/*") if p.suffix == ".ckpt"]
     if pattern:
-        ckpts = [ckpt for ckpt in ckpts if pattern in ckpt.stem]
+        ckpts = [ckpt for ckpt in ckpts if pattern in Path(ckpt).stem]
     if return_first:
         ckpt = ckpts[0]
         print("Loading checkpoint: ", ckpt)
